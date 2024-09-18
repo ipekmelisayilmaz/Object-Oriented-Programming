7e90d93d44d2cb05ae1045e4b92a682c66a77f8e
İpek Melisa Yılmaz <ipek.yilmaz3@ogr.dpu.edu.tr>
2024-09-18 21:31:00 +0300

    inheritance

--- Cpp/vehicle/main.cpp
+++ Cpp/vehicle/main.cpp
@@ -7,8 +7,6 @@ protected:
    string manufacturer;
    int year;
    string color;
-
-
 public:
 string getManufacturer()
 {
@@ -42,6 +40,7 @@ Vehicle()
 Vehicle(string manufacturer,int year,string color): Vehicle() //parametresiz fonksiyona çağrı yaptıldı kod tekrarına düşmemek amacıyla
 
 {
+    cout<<"\nAn instance with full parameters has been derived from Vehicle"<<endl;
     this->manufacturer=manufacturer;
     this->year=year;
     this->color=color;
@@ -78,22 +77,82 @@ void openSunroof()
     cout<< "The sunroof has been opened. " <<endl;
 }
 };
+class Bus: public Vehicle
+{
+public:
+Bus(string manufacturer,int year,string color)
+:Vehicle(manufacturer,year,color)//Base class'a parametre gönderimi
+{
+     cout<<"\nAn instance with full parameters has been created from Bus Class"<<endl;
+
+}
+void scheduling()
+{
+    cout<<"Scheduled"<<endl;
+}
+};
+class SchoolBus: public Bus
+{
+public:
+SchoolBus(string manufacturer,int year,string color)
+:Bus(manufacturer,year,color)
+{
+  cout<<"\nAn instance with full parameters has been created from SchoolBus Class"<<endl;
+}
+void getFare()
+{
+    cout<<"getFare() has been called"<<endl;
+}
+
+};
+class Truck :public Vehicle
+{
+public:
+ Truck(string manufacturer,int year,string color)
+ :Vehicle(manufacturer,year,color)
+{
+    cout<<"\nAn instance with full parameters has been created from Truck Class"<<endl;
+    
+}
+void transport()
+{
+    cout<<"transport() has been called"<<endl;
+}
+};
 int main()
 {
     //önce base classın yapıcı metodu çalışacak sonra derived classın yapıcı metodu çalışacak
-
-
     
     Vehicle item2 =  Vehicle("Skoda",2019,"White");//vehicle'den üretildi
-     /*std::*/cout<< "Vehicle " << item2.getManufacturer() << /*std::*/endl;
+     cout<< "Vehicle " << item2.getManufacturer() << endl;
 
     Car item1 =  Car();//car'dan üretildi
     item1.setManufacturer("TOGG") ;//araç bilgisi verildi
-    /*std::*/cout<< "Vehicle " << item1.getManufacturer() /*ekrana yazdırıldı*/ << /*std::*/endl;
+    cout<< "Vehicle " << item1.getManufacturer() /*ekrana yazdırıldı*/ << endl;
+    item1.openSunroof();
+    item1.start();
 
-      Car item3 =  Car("Opel",2020,"blue");//car'dan üretildi
-    item3.setManufacturer("TOGG") ;//araç bilgisi verildi
-    /*std::*/cout<< "Vehicle " << item3.getManufacturer() /*ekrana yazdırıldı*/ << /*std::*/endl;
+
+    Car item3 =  Car("Opel",2020,"blue");//car'dan üretildi
+    cout<< "Vehicle " << item3.getManufacturer() /*ekrana yazdırıldı*/ << endl;
+    item3.openSunroof();
+
+     Bus item4 =  Bus("Megane",2022,"black");
+    cout<< "Vehicle " << item4.getManufacturer() /*ekrana yazdırıldı*/ << endl;
+    item4.scheduling();
+    
+    SchoolBus item5 = SchoolBus("Isuzu",2015,"Yellow");
+    cout<< "Vehicle " << item5.getManufacturer() /*ekrana yazdırıldı*/ << endl;
+    item5.scheduling();
+    item5.getFare();
+
+    Truck item6 = Truck("RangeRover",2022,"white");
+    cout<< "Vehicle " << item6.getManufacturer() /*ekrana yazdırıldı*/ << endl;
+    item6.transport();
+    item6.start();
+    item6.stop();
+    cout<<item6.getManufacturer()<<endl;
+
     char c = getchar();
 
     return 0;
