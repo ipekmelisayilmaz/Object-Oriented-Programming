ff0c6f4d970e9193c5f709eadeac842770bae8e5
İpek Melisa Yılmaz <ipek.yilmaz3@ogr.dpu.edu.tr>
2024-09-24 23:07:26 +0300

    Csharp

---
+++ Csharp/StackApp/StackApp/Contract/IStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp.Contract
+{
+    //Arayüzler kuralları koyar, detaylarla ilgilenmez , alt sınıflar detayları organize eder
+    public interface IStack
+    {
+        void Push(Object item);
+        Object Pop();
+        Object Peek();
+    }
+}
---
+++ Csharp/StackApp/StackApp/Models/LinkedListStack.cs
@@ -0,0 +1,37 @@
+﻿using StackApp.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp.Models
+{
+    public class LinkedListStack : IStack
+    {
+        private LinkedList<object> _collection;//Bu liste herhangi bir türde (object türünde) nesneleri tutabilen bir bağlantılı listedir
+                                               //Bu ifadenin başlatılması gerekir, kurucu metot içerisinde bu işlem gerçekleştirilebilir
+        public LinkedListStack()
+        {
+            _collection = new LinkedList<object>();//bir örneği oluşturuldu
+        }
+        public object Peek()
+        {
+            return _collection.First.Value;
+        }
+
+        public object Pop()
+        {
+            var temp = _collection.First.Value;
+            _collection.RemoveFirst();//void bir fonk oldugundan değer döndürmez.
+            return temp;                        //silinen elemanı geri döndürmek için farklı bir değişkende tutulmalı
+         
+        }
+
+        public void Push(object item)
+        {
+            _collection.AddFirst(item);//AddFirst fonksiyonu, LinkedList<T> sınıfında kullanılan bir metottur.
+                                       //Bu metot, bir bağlantılı listenin başına yeni bir öğe eklemek için kullanılır
+        }
+    }
+}
